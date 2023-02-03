ff9f94e287d1cad90e3f25c2a073fab6f7aa972b
Goktug YILDIZ <102765970+GktgYildiz@users.noreply.github.com>
2023-02-03 19:22:07 +0300

    Add files via upload

---
+++ Winform24/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Winform24
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.northwindDataSet = new Winform24.NorthwindDataSet();
+            this.northwindDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.northwindDataSet1 = new Winform24.NorthwindDataSet();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productsTableAdapter = new Winform24.NorthwindDataSetTableAdapters.ProductsTableAdapter();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.güncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Cyan;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(455, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // northwindDataSet
+            // 
+            this.northwindDataSet.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // northwindDataSetBindingSource
+            // 
+            this.northwindDataSetBindingSource.DataSource = this.northwindDataSet;
+            this.northwindDataSetBindingSource.Position = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.DataSource = this.productsBindingSource;
+            this.listBox1.DisplayMember = "ProductName";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(455, 472);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.ValueMember = "ProductID";
+            // 
+            // northwindDataSet1
+            // 
+            this.northwindDataSet1.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.northwindDataSet1;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.güncelleToolStripMenuItem,
+            this.silToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(121, 48);
+            // 
+            // güncelleToolStripMenuItem
+            // 
+            this.güncelleToolStripMenuItem.Name = "güncelleToolStripMenuItem";
+            this.güncelleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.güncelleToolStripMenuItem.Text = "Güncelle";
+            this.güncelleToolStripMenuItem.Click += new System.EventHandler(this.güncelleToolStripMenuItem_Click);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(476, 552);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource northwindDataSetBindingSource;
+        private NorthwindDataSet northwindDataSet;
+        private System.Windows.Forms.ListBox listBox1;
+        private NorthwindDataSet northwindDataSet1;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private NorthwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem güncelleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+    }
+}
+
---
+++ Winform24/Form2.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Winform24
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+        DataRowView _gelen;
+        public Form2(DataRowView gelen, string islem)
+        {
+            InitializeComponent();
+            _gelen = gelen;
+            if (islem == "Guncelle")
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+
+            }
+            else if (islem == "Sil")
+            {
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+        }
+
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Winform24.Properties.Settings.NorthwindConnectionString"].ConnectionString);
+        public delegate void UrunEkleEventHandler();
+        public event UrunEkleEventHandler UrunEkle;
+        //event ile bu tetiklenince kendi üstüne alıyor , event eklemeye gerek kalmıyor
+
+        public delegate void UrunGuncelleEventHandler();
+        public event UrunGuncelleEventHandler UrunGuncelle;
+
+
+        public delegate void UrunSilEventHandler();
+        public event UrunSilEventHandler UrunSil;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'northwindDataSet2.Categories' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.categoriesTableAdapter.Fill(this.northwindDataSet2.Categories);
+            if (_gelen != null)
+            {
+                //guncelle: eğer gelen veri boş değilse güncelleme var
+                textBox1.Text = _gelen["ProductName"].ToString();
+                textBox2.Text = _gelen["UnitPrice"].ToString();
+                textBox3.Text = _gelen["UnitsInStock"].ToString();
+                textBox4.Text = _gelen["UnitsOnOrder"].ToString();
+                comboBox1.SelectedValue = _gelen["CategoryId"];
+            }
+            else
+            {
+                MessageBox.Show("Lütfen yönergelere uygun bir biçimde seçim yapınız.");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_gelen != null)
+            {
+                //güncelle
+                string sorguGuncelle = "Update Products Set ProductName=@name, UnitPrice=@price, UnitsInStock=@stock, UnitsOnOrder=@order, CategoryId=@catId where ProductId = @id";
+                using (SqlCommand cmd = new SqlCommand(sorguGuncelle, conn))
+                {
+                    cmd.Parameters.AddWithValue("@name", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@price", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@stock", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@order", textBox4.Text);
+                    cmd.Parameters.AddWithValue("@catId", comboBox1.SelectedValue);
+
+                    cmd.Parameters.AddWithValue("@id", _gelen["ProductId"]);
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                        UrunGuncelle();
+                        this.Close();
+                    }
+                    else
+                    {
+                        conn.Close();
+                    }
+                }
+
+            }
+            else
+            {
+                //ekle
+                string sorguInsert = "Insert Products(ProductName,UnitPrice,UnitsInStock,UnitsOnOrder,CategoryId) Values (@name, @price, @stock, @order, @catId)";
+                using (SqlCommand cmd = new SqlCommand(sorguInsert, conn))
+                {
+                    cmd.Parameters.AddWithValue("@name", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@price", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@stock", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@order", textBox4.Text);
+                    cmd.Parameters.AddWithValue("@catId", comboBox1.SelectedValue);
+
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                        MessageBox.Show("Kayıt Başarıyla Oluşturuldu");
+                        UrunEkle();
+                        this.Close();
+                    }
+                    else
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_gelen != null)
+            {
+                string sorguDelete = "Delete from Products where ProductId=@id";
+                using (SqlCommand cmd = new SqlCommand(sorguDelete, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", _gelen["ProductId"]);
+                    try
+                    {
+                        DialogResult res = MessageBox.Show("Ürünü silmek istediğinize emin misiniz?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        if (res == DialogResult.OK)
+                        {
+                            if (conn.State == ConnectionState.Closed)
+                            {
+                                conn.Open();
+                                cmd.ExecuteNonQuery();
+                                conn.Close();
+                                UrunSil();
+                                MessageBox.Show("Ürün başarıyla silindi!");
+                                this.Close();
+                            }
+                            else
+                            {
+                                conn.Close();
+                            }
+                        }
+                        if (res == DialogResult.Cancel)
+                        {
+                            this.Close();
+
+                        }
+
+
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("beklenmedik bir hata oluştu!");
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Silinecek ürünün Id'si boş. Lütfen kontrol ediniz");
+            }
+
+        }
+    }
+}
